79c216ae8caecd832676527061b611375a2efd38
Indie19 <indie.cine7@gmail.com>
2019-05-22 15:02:57 -0500

    import addBanksPage and listBanksPage

--- ionic-bancos/src/pages/home/home.ts
+++ ionic-bancos/src/pages/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddBanksPage } from '../add-banks/add-banks';
+import { ListBanksPage } from '../list-banks/list-banks';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  addBanksPage: any = AddBanksPage;
+  listBanksPage: any = ListBanksPage;
 
   constructor(public navCtrl: NavController) {
 
